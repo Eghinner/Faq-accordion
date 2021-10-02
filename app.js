a5dadc1985c9d096fd5fa03a9a707d5705fc0d64
Eghinner <eghinner@gmail.com>
2021-10-02 01:19:58 -0700

    just one dropdown

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-
-var paraph = document.querySelectorAll("p");
 var quest = document.querySelectorAll(".quest");
+var paraph = document.querySelectorAll("p");
 var arrow = document.querySelectorAll(".arrow");
 
 for(let i in quest){
 	quest[i].addEventListener("click", () => {
-		paraph[i]	.classList.toggle("mystyle");
+		paraph[i]	.classList.toggle("mystyle");		
 		arrow[i]	.classList.toggle("rotate");
 		quest[i]	.classList.toggle("black");
+		for(let o in quest){
+			if (quest[o] == quest[i]) {
+				continue;
+			} else {
+				paraph[o].classList.remove("mystyle");
+				arrow[o].classList.remove("rotate");
+				quest[o].classList.remove("black");
+			}	
+		}
 	});
 }
